73a750be48698eff652b7e6f8c22f60116e8ebfa
Steven Burgart <skburgart@gmail.com>
2018-07-13 15:55:31 -0400

    Reorg stuffs

--- crypto.go
+++ crypto.go
@@ -10,17 +10,16 @@ import (
 )
 
 var (
-	CLIENT_KEY = "eJUWrzRv34qFSaYk"
-	SECRET     = "Cyu5jcR4zyK6QEPn1hdIGXB5QIDAQABMA0GC"
-)
-
-var ECOVACS_PUBLIC_KEY = `
+	CLIENT_KEY         = "eJUWrzRv34qFSaYk"
+	SECRET             = "Cyu5jcR4zyK6QEPn1hdIGXB5QIDAQABMA0GC"
+	ECOVACS_PUBLIC_KEY = `
 -----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDb8V0OYUGP3Fs63E1gJzJh+7iq
 eymjFUKJUqSD60nhWReZ+Fg3tZvKKqgNcgl7EGXp1yNifJKUNC/SedFG1IJRh5hB
 eDMGq0m0RQYDpf9l0umqYURpJ5fmfvH/gjfHe3Eg/NTLm7QEa0a0Il2t3Cyu5jcR
 4zyK6QEPn1hdIGXB5QIDAQAB
 -----END PUBLIC KEY-----`
+)
 
 func encrypt(message string) string {
 	block, _ := pem.Decode([]byte(ECOVACS_PUBLIC_KEY))
--- main.go
+++ main.go
@@ -5,5 +5,15 @@ import (
 )
 
 func main() {
-	log.Println(encrypt("test"))
+	log.Println("starting go-deebot service")
+	init_api()
+}
+
+func init_api() {
+	log.Println("initializing deebot api")
+
+}
+
+func call_main_api(endpoint string, args map[string]string) {
+
 }
