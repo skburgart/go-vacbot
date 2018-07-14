8c6b9ce7115976b4e73af10b6b2f3f86cc881473
Steven Burgart <skburgart@gmail.com>
2018-07-14 10:47:24 -0400

    Added signing function

--- crypto.go
+++ crypto.go
@@ -9,6 +9,8 @@ import (
 	"encoding/pem"
 	"fmt"
 	"log"
+	"sort"
+	"time"
 )
 
 var (
@@ -42,6 +44,60 @@ func encrypt(message string) string {
 
 	return base64.StdEncoding.EncodeToString(cipherText)
 }
+
+func meta() map[string]string {
+	return map[string]string{
+		"country":    country,
+		"lang":       lang,
+		"deviceId":   device_id,
+		"appCode":    app_code,
+		"appVersion": app_version,
+		"channel":    channel,
+		"deviceType": device_type,
+	}
+}
+
+func sign(args map[string]string) map[string]string {
+	args["authTimespan"] = fmt.Sprintf("%v", time.Now().UnixNano()/1000000)
+	args["authTimeZone"] = "GMT-8"
+	args["authAppkey"] = CLIENT_KEY
+
+	textToSign := CLIENT_KEY
+
+	// prepare signature map
+	signMap := meta()
+	for k, v := range args {
+		signMap[k] = v
+	}
+
+	// construct message to sign
+	for _, k := range sortedKeys(args) {
+		textToSign += fmt.Sprintf("%s=%s", k, signMap[k])
+	}
+
+	args["authSign"] = md5hash(textToSign)
+
+	return args
+}
+
+func sortedKeys(m map[string]string) []string {
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+//    sign_on = self.meta.copy()
+//    sign_on.update(result)
+//    sign_on_text = EcoVacsAPI.CLIENT_KEY + ''.join(
+//        [k + '=' + str(sign_on[k]) for k in sorted(sign_on.keys())]) + EcoVacsAPI.SECRET
+//
+//    result['authAppkey'] = EcoVacsAPI.CLIENT_KEY
+//    result['authSign'] = self.md5(sign_on_text)
+//    print("result after: ", result)
+//    return result
 
 func md5hash(text string) string {
 	return fmt.Sprintf("%x", md5.Sum([]byte(text)))
--- main.go
+++ main.go
@@ -18,18 +18,17 @@ func init_api() {
 		"password": encrypt(password_hash),
 	}
 	call_main_api("user/login", loginMap)
-
 }
 
 var (
-	MAIN_URL = "https://eco-{country}-api.ecovacs.com/v1/private/{country}/{lang}/{deviceId}/{appCode}/{appVersion}/{channel}/{deviceType}"
+	MAIN_URL = "https://eco-%s-api.ecovacs.com/v1/private/%s/%s/%s/%s/%s/%s/%s"
 )
 
 func call_main_api(endpoint string, args map[string]string) {
 	args["requestId"] = md5hash(time.Now().String())
-	log.Println(get_main_url())
+	sign(args)
 }
 
 func get_main_url() string {
-	return fmt.Sprintf("https://eco-%s-api.ecovacs.com/v1/private/%s/%s/%s/%s/%s/%s/%s", country, country, lang, device_id, app_code, app_version, channel, device_type)
+	return fmt.Sprintf(MAIN_URL, country, country, lang, device_id, app_code, app_version, channel, device_type)
 }
