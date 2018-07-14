9d5c7949d53aed67a8b4c1db8d3ba2d2a0cccc07
Steven Burgart <skburgart@gmail.com>
2018-07-14 14:42:39 -0400

    Get user access token

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -17,11 +19,14 @@ func main() {
 func init_api() {
 	log.Println("initializing deebot api")
 	uid, access_token := login(email, password_hash)
-	auth_code := get_auth_code(uid, access_token)
+	authCode := get_auth_code(uid, access_token)
+	userId, userAccessToken := get_user_access_token(uid, authCode)
+	log.Printf("userId '%s' userAccessToken '%s'\n", userId, userAccessToken)
 }
 
 var (
 	MAIN_URL = "https://eco-%s-api.ecovacs.com/v1/private/%s/%s/%s/%s/%s/%s/%s"
+	USER_URL = "https://users-%s.ecouser.net:8000/user.do"
 )
 
 func login(email, passwordHash string) (string, string) {
@@ -96,10 +101,58 @@ func call_main_api(endpoint string, args map[string]string) map[string]interface
 	return result
 }
 
-func call_user_api(endpoint string, args map[string]string) map[string]interface{} {
-	return nil
+func call_user_api(function string, args map[string]string) map[string]interface{} {
+	args["todo"] = function
+
+	jsonArgs, err := json.Marshal(args)
+	if err != nil {
+		log.Fatal("error marshalling user api json")
+	}
+
+	resp, err := http.Post(get_user_url(), "application/json", bytes.NewBuffer(jsonArgs))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	resp_body, _ := ioutil.ReadAll(resp.Body)
+
+	var result map[string]interface{}
+	json.Unmarshal(resp_body, &result)
+	return result
+}
+
+func call_login_by_it_token(uid, auth_code string) map[string]interface{} {
+	args := map[string]string{
+		"country":  strings.ToUpper(country),
+		"resource": resource,
+		"realm":    realm,
+		"userId":   uid,
+		"token":    auth_code,
+	}
+
+	return call_user_api("loginByItToken", args)
+}
+
+func get_user_access_token(uid, authCode string) (string, string) {
+	responseJson := call_login_by_it_token(uid, authCode)
+
+	result := responseJson["result"].(string)
+	if result != "ok" {
+		log.Fatal("get user access token error")
+	}
+	log.Println("get user access token successful")
+
+	userId := responseJson["userId"].(string)
+	userAccessToken := responseJson["token"].(string)
+
+	return userId, userAccessToken
 }
 
 func get_main_url() string {
 	return fmt.Sprintf(MAIN_URL, country, country, lang, device_id, app_code, app_version, channel, device_type)
+}
+
+func get_user_url() string {
+	return fmt.Sprintf(USER_URL, continent)
 }
