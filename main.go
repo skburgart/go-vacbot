4140ef23ee3695999fa0daa5f91e5ce8fcb71373
Steven Burgart <skburgart@gmail.com>
2018-07-14 13:53:01 -0400

    Some cleanup

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
@@ -16,18 +16,35 @@ func main() {
 
 func init_api() {
 	log.Println("initializing deebot api")
-	loginMap := map[string]string{
-		"account":  encrypt(email),
-		"password": encrypt(password_hash),
-	}
-	call_main_api("user/login", loginMap)
+	access_token := login(email, password_hash)
+
+	log.Printf("access token %s\n", access_token)
 }
 
 var (
 	MAIN_URL = "https://eco-%s-api.ecovacs.com/v1/private/%s/%s/%s/%s/%s/%s/%s"
 )
 
-func call_main_api(endpoint string, args map[string]string) {
+func login(email, passwordHash string) string {
+	loginMap := map[string]string{
+		"account":  encrypt(email),
+		"password": encrypt(password_hash),
+	}
+	responseJson := call_main_api("user/login", loginMap)
+
+	code := responseJson["code"].(string)
+	if code != "0000" {
+		log.Fatal("login error")
+	}
+	log.Println("login successful")
+
+	data := responseJson["data"].(map[string]interface{})
+	accessToken := data["accessToken"].(string)
+
+	return accessToken
+}
+
+func call_main_api(endpoint string, args map[string]string) map[string]interface{} {
 	args["requestId"] = md5hash(time.Now().String())
 	sign(args)
 
@@ -36,8 +53,7 @@ func call_main_api(endpoint string, args map[string]string) {
 	url := fmt.Sprintf("%s/%s", get_main_url(), endpoint)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Print(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 
 	q := req.URL.Query()
@@ -50,14 +66,15 @@ func call_main_api(endpoint string, args map[string]string) {
 	resp, err := client.Do(req)
 
 	if err != nil {
-		log.Print(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 
 	defer resp.Body.Close()
 	resp_body, _ := ioutil.ReadAll(resp.Body)
 
-	log.Printf(string(resp_body))
+	var result map[string]interface{}
+	json.Unmarshal(resp_body, &result)
+	return result
 }
 
 func get_main_url() string {
