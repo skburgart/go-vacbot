a1faabce1032af1e7a3036071571364cbb42b911
Steven Burgart <skburgart@gmail.com>
2018-07-15 10:53:54 -0400

    Add basic commands

---
+++ vacbot/commands.go
@@ -0,0 +1,11 @@
+package vacbot
+
+var (
+	COMMAND_MOVE_FORWARD    = `<ctl td="Move"><move action="forward"/></ctl>`
+	COMMAND_MOVE_SPIN_LEFT  = `<ctl td="Move"><move action="SpinLeft"/></ctl>`
+	COMMAND_MOVE_SPIN_RIGHT = `<ctl td="Move"><move action="SpinRight"/></ctl>`
+	COMMAND_TURN_AROUND     = `<ctl td="Move"><move action="TurnAround"/></ctl>`
+	COMMAND_STOP_MOVING     = `<ctl td="Move"><move action="stop"/></ctl>`
+
+	COMMAND_GET_BATTERY_STATE = `<ctl td="GetBatteryInfo" />`
+)
